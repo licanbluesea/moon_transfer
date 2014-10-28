f247a9f5d1b5bae45f2a7196fc3ac73e75062bd0
licanming <licanming@kingsoft.com>
2014-10-28 17:39:01 +0800

    update jsonp

--- assets/js/page_transfer.js
+++ assets/js/page_transfer.js
@@ -3,26 +3,38 @@
  */
 (function($){
     $.extend({moon:{
-        game_url:"http://moon.xoyobox.com",
+        game_url:"http://www.licanbluesea.com:3000/migration/",
+        session_url:"",
+        isthird:"",
         init:function(){
+            var _that = this;
             PageUI.prototype.log_status=this.log_add;
             PageUI.prototype.loged_status=this.loged_add;
-            this.page_ui();
-            window.pageUI = new PageUI();
+            this.game_sessionId(function(){
+                _that.game_isthird(function($data){
+                        _that.isthird = $data.tips.thirdparty*1;
+                        _that.page_ui();
+                        window.pageUI = new PageUI();
+                    });
+
+            });
         },
         log_add:function(){
-            console.log("专题没有账号登录");
+
             _this = $.moon;
             _this.title_change();
             $(".step_input > div").hide();
             $(".step_a1").show();
         },
         page_ui:function(){
+            var _that = this;
             $(".act_reg").click(function(){
+
                 $(".step_input > div").hide();
                 $(".step_a2").show();
             });
             $(".act_log").click(function(){
+
                 $(".step_input > div").hide();
                 $(".step_a3").show();
             });
@@ -30,12 +42,61 @@
                 $(".step_input > div").hide();
                 $(".step_a1").show();
             });
+            $(".createGameaccount_btn").click(function(){
+                $(".step_input > div").hide();
+                $(".step_b2").show();
+            });
+            $(".subGameaccount_btn").click(function(){
+                _that.add_gameAccount();
+            });
         },
         loged_add:function(){
-            console.log("专题有账号登录");
+            //console.log("专题有账号登录");
+
+            var _that = $.moon,_html = "";
             $(".step_input > div").hide();
+            _that.game_info(function($data){
+                if($data.status > 0){
+                    $.each($data.tips.account,function(i,n){
+                        _html += "<option value="+ n+" >"+ n+"</option>"
+                    });
+                    $(".step_b1  .game_account").append(_html);
+                    $(".step_b1").show();
+                }
+                //console.log($data);
+            });
 
+        },
+        add_gameAccount:function(){
+            var game_account = "",game_pass = "",game_capcha =""
+            if($("input[name='game_account']").val()==""){
+                alert("請輸入遊戲賬號！");
+                return false;
+            }else{
+                game_account = $("input[name='game_account']").val();
+            }
+            if($("input[name='game_pass']").val()==""){
+                alert("請輸入遊戲密碼！");
+                return false;
+            }
+            if($("input[name='game_pass']").val()!=$("input[name='game_repass']").val()){
+                alert("請輸入相同的密碼！");
+                return false;
+            }else{
+                game_pass = $("input[name='game_pass']").val();
+            }
+            if($("input[name='game_capcha']").val()==""){
+                alert("請輸入驗證碼！");
+                return false;
+            }else{
+                game_capcha = $("input[name='game_capcha']").val();
+            }
+            this.sub_gameAccount({"esid":this.session_url,"account":game_account,"pass":game_pass,"repass":game_pass,"vcode":game_capcha})
+        },
+        sub_gameAccount:function($params){
+            this.game_creat($params,function($data){
 
+            });
         },
         title_change:function($title){
             $(".step_title").removeClass("").addClass(".step_title");
@@ -47,6 +108,65 @@
                 default :
 
             }
+        },
+        game_isthird:function($fn){
+            var _that = this;
+            $.ajax({
+                'type': 'GET',
+                'dataType': 'jsonp',
+                'data':{"esid":_that.session_url},
+                'url':this.game_url+"thirdparty/",
+                'success':function($data){
+                    if($fn){
+                        $fn($data);
+                    }
+                }
+            });
+        },
+        game_info:function($fn){
+            var _that = this;
+            $.ajax({
+                'type': 'GET',
+                'dataType': 'jsonp',
+                'data':{"esid":_that.session_url},
+                'url':this.game_url+"acclist/",
+                'success':function($data){
+                    if($fn){
+                        $fn($data);
+                    }
+                }
+            });
+        },
+        game_creat:function ($parms,$fn){
+            var _that = this
+            $.ajax({
+                'type': 'GET',
+                'dataType': 'jsonp',
+                'data':$parms,
+                'url':this.game_url+"reg/",
+                'success':function($data){
+                    _that.session_url = $data
+                    if($fn){
+
+                        $fn();
+                    }
+                }
+            });
+        },
+        game_sessionId:function($fn){
+            var _that = this
+            $.ajax({
+                'type': 'GET',
+                'dataType': 'jsonp',
+                'url':'https://hk.xoyobox.com/index.php?r=ExApi/GetEsessionId',
+                'success':function($data){
+                    _that.session_url = $data.data
+                    if($fn){
+
+                        $fn();
+                    }
+                }
+            })
         }
     }});
 })(jQuery);
--- index.js
+++ index.js
@@ -7,6 +7,15 @@ var lessMiddleware = require('less-middleware');
 var fs = require('fs');
 
 var app = module.exports = express();
+app.all('*', function(req, res, next) {
+    res.header("host","www.licanbluesea.com")
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    if(req.method=="OPTIONS") res.send(200);
+    else  next();
+});
 app.use( lessMiddleware(__dirname +'/',{
     "force":true
 }));
@@ -22,7 +31,10 @@ app.get('/list', function(req, res){
 app.get('/news', function(req, res){
     res.end(output("news"));
 });
-
+app.get('/migration/*',function(req,res){
+    var _fun = req.query.callback
+    res.end(_fun+"("+JSON.stringify(routers(req.params[0]))+")");
+});
 app.listen(3000,function(){
     console.log("sever at :3000")
 });
@@ -33,5 +45,22 @@ function output($fs){
     });
     return _html;
 }
+function routers($params){
+    var _url = $params.replace("\/","");
+    switch(_url){
+        case 'acclist':
+            var _obj = {"status":1,"tips":{account:["user1", "user2", "user3"]}}
+            return _obj;
+            break;
+        case 'thirdparty':
+            var _obj = {"status":1,"tips":{"thirdparty":0}};
+            return _obj;
+            break;
+        case 'reg':
+            var _obj = {"status":1,"tips":"賬號創建成功！"};
+            return _obj;
+            break;
+    }
+};
 //var log_list = [{"title":"多玩魔兽数据库9月12日更新了5.4新增的武器和装备","time":"2014 10.01","url":"http:jx3.xoyo.com"},{"title":"多玩魔兽数据库9月12日更新了5.4新增的武器和装备","time":"2014 10.01","url":"http:jx3.xoyo.com"},{"title":"多玩魔兽数据库9月12日更新了5.4新增的武器和装备","time":"2014 10.01","url":"http:jx3.xoyo.com"},{"title":"多玩魔兽数据库9月12日更新了5.4新增的武器和装备","time":"2014 10.01","url":"http:jx3.xoyo.com"}];
 
